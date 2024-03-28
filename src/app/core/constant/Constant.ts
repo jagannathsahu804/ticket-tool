75ab449b95435e5887bcf31f08b4e1cd956969dc
Jagannath Sahu <jagannathsahu804@gmail.com>
2024-03-27 21:00:21 -0500

    folder structure done

---
+++ src/app/core/constant/Constant.ts
@@ -0,0 +1,9 @@
+export const Constant = {
+    API_END_Point : {
+        GET_DEPARTMENT: 'GetDepartments',
+        CREATE_DEPARTMENT: 'CreateDepartmnet'
+    },
+    VALIDATION_MESSAGE:{
+        REQUIRED: 'This is Required'
+    }
+}
